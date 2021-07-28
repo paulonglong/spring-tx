350c4411ed70296803cf1d6df9f527f0c220b5bc
LAPTOP-QEJCS1J7\wlli <wlli@wisedu.com>
2021-07-28 12:22:05 +0800

    声明式事务

---
+++ src/main/java/com/itranswarp/learnjava/AppConfig.java
@@ -0,0 +1,67 @@
+package com.itranswarp.learnjava;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.itranswarp.learnjava.service.UserService;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan
+@EnableTransactionManagement
+@PropertySource("jdbc.properties")
+public class AppConfig {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        System.out.println(userService.getClass());
+        userService.bathRegister();
+        ((ConfigurableApplicationContext)context).close();
+    }
+
+    @Value("${jdbc.url}")
+    String jdbcUrl;
+
+    @Value("${jdbc.username}")
+    String jdbcUsername;
+
+    @Value("${jdbc.password}")
+    String jdbcPassword;
+
+    @Bean
+    DataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(jdbcUsername);
+        config.setPassword(jdbcPassword);
+        config.addDataSourceProperty("autoCommit", "false");
+        config.addDataSourceProperty("connectionTimeout", "5");
+        config.addDataSourceProperty("idleTimeout", "60");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    JdbcTemplate createJdbcTemplate(@Autowired DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    PlatformTransactionManager createTxManager(@Autowired DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
---
+++ src/main/java/com/itranswarp/learnjava/DbInitializer.java
@@ -0,0 +1,29 @@
+package com.itranswarp.learnjava;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+
+public class DbInitializer {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	PlatformTransactionManager txManager;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("init table...");
+        // jdbcTemplate.update("DROP TABLE users");
+		jdbcTemplate.update("CREATE TABLE IF NOT EXISTS users (" + //
+				"id BIGINT IDENTITY NOT NULL," + //
+				"email VARCHAR(100) NOT NULL," + //
+				"password VARCHAR(100) NOT NULL," + //
+				"name VARCHAR(100) NOT NULL," + //
+				"PRIMARY KEY (id)," + //
+				"UNIQUE (email))");
+	}
+}
